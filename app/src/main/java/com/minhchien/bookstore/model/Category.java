5221359dbff193e531291f606912608f610b2c3b
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-06-03 23:05:22 +0700

    update 23:05, 6/3/2024

---
+++ app/src/main/java/com/minhchien/bookstore/model/Category.java
@@ -0,0 +1,33 @@
+package com.minhchien.bookstore.model;
+
+import java.util.List;
+
+public class Category {
+    private String category;
+    private List<Book> list;
+
+    public Category(String category) {
+        this.category = category;
+    }
+
+    public Category(String category, List<Book> list) {
+        this.category = category;
+        this.list = list;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<Book> getList() {
+        return list;
+    }
+
+    public void setList(List<Book> list) {
+        this.list = list;
+    }
+}
