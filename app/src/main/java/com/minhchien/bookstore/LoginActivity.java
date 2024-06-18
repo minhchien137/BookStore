4fea7e9d641040d5b9c1e92b204ee8e08dd78ce8
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-06-18 14:36:26 +0700

    Card Data Access Object done 15:40

--- app/src/main/java/com/minhchien/bookstore/LoginActivity.java
+++ app/src/main/java/com/minhchien/bookstore/LoginActivity.java
@@ -10,9 +10,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.minhchien.bookstore.admin.AdminMenuActivity;
 import com.minhchien.bookstore.model.User;
+import com.minhchien.bookstore.sharepreference.Constants;
+import com.minhchien.bookstore.sharepreference.PreferenceManager;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,7 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     EditText txtPhone,txtPass;
-    Button btnLogin, btnRegister;
+    TextView tvSignupLogin;
+    Button btnLogin;
     private User user;
     private SharedPreferences sharedPreferences;
 
@@ -28,5 +38,90 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Users");
+        txtPhone = (EditText) findViewById(R.id.sodienthoaiLogin);
+        txtPass = (EditText) findViewById(R.id.matkhauLogin);
+        btnLogin = (Button) findViewById(R.id.loginButton);
+        tvSignupLogin = (TextView) findViewById(R.id.signupText);
 
-}}
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = txtPhone.getText().toString().trim();
+                String pass = txtPass.getText().toString().trim();
+                if(phone.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Số điện thoại không được để trống!", Toast.LENGTH_SHORT).show();
+                } else if (pass.length() < 6) {
+                    Toast.makeText(LoginActivity.this, "Mật khẩu không được để trống!", Toast.LENGTH_SHORT).show();
+                } else{
+                    myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(phone)){
+                                final User user = snapshot.child(phone).getValue(User.class);
+                                if(user.getPassword().equals(pass)){
+                                    Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                                    writeToSharedPreferences(user);
+                                    //is admin
+                                    if(user.getIsAdmin() == 1){
+                                        Intent adminMenu = new Intent(getApplicationContext(), AdminMenuActivity.class);
+                                        //adminMenu.putExtra(USER_KEY,user);
+                                        startActivity(adminMenu);
+                                    }
+                                    else{
+                                        //Main menu Activity
+                                        //Gui user
+                                        Intent menu = new Intent(getApplicationContext(),MenuActivity.class);
+                                        //menu.putExtra(USER_KEY,user);
+                                        startActivity(menu);
+                                    }
+
+                                }
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Đăng nhập thất bại!\nVui lòng kiểm tra lại", Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+        tvSignupLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeToRegisterActivity();
+            }
+        });
+
+
+
+
+}
+    private void writeToSharedPreferences(User user){
+        PreferenceManager preferenceManager = new PreferenceManager(getApplicationContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+//        sharedPreferences = getSharedPreferences("book_store", Context.MODE_PRIVATE);
+//        SharedPreferences.Editor  editor = sharedPreferences.edit();
+        String phone = user.getPhoneNumber();
+        int isAdmin = user.getIsAdmin();
+        preferenceManager.putString(Constants.LOGIN_PHONE,phone);
+        preferenceManager.putInt(Constants.LOGIN_IS_ADMIN,isAdmin);
+//        editor.putString("phone",phone);
+//        editor.putInt("isAdmin",isAdmin);
+//        editor.commit();
+    }
+
+    private void changeToRegisterActivity(){
+        Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/com/minhchien/bookstore/database/BookDao.java
+++ app/src/main/java/com/minhchien/bookstore/database/BookDao.java
@@ -1,4 +1,108 @@
 package com.minhchien.bookstore.database;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.minhchien.bookstore.model.Book;
+
+import java.util.HashMap;
+
 public class BookDao {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    Book book;
+    Context context;
+    public BookDao(Context context) {
+        // Write a message to the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Books");
+        this.context = context;
+    }
+    public boolean addBook(Book book){
+        final boolean[] result = new boolean[1];
+        //Add to DB
+        String id = myRef.push().getKey();
+        book.setIdBook(id);
+        myRef.child(book.getIdBook()).setValue(book).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    result[0] = true;
+                    Toast.makeText(context, "Thêm sách thành công", Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    result[0] = false;
+                    Toast.makeText(context, "Thêm sách thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return result[0];
+    }
+    public boolean updateBook(String idBook, String titleBook, String author, String categoryBook, String imgURLBook, String companyBook, String versionBook, int yearBook, int priceBook, int inStockBook, String descriptionBook, int isActive){
+        final boolean[] result = new boolean[1];
+        HashMap Book = new HashMap();
+        Book.put("id",idBook);
+        Book.put("title", titleBook);
+        Book.put("author",author);
+        Book.put("category",categoryBook);
+        Book.put("price",priceBook);
+        Book.put("year",yearBook);
+        Book.put("inStock",inStockBook);
+        Book.put("description",descriptionBook);
+        Book.put("imgURL",imgURLBook);
+        Book.put("company", companyBook);
+        Book.put("version", versionBook);
+        Book.put("isActive",isActive);
+        myRef = database.getReference("Books");
+        myRef.child(idBook).updateChildren(Book).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(context, "Cập nhật thành công", Toast.LENGTH_SHORT).show();
+                    result[0] = true;
+                }
+                else{
+                    Toast.makeText(context, "Cập nhật thất bại", Toast.LENGTH_SHORT).show();
+                    result[0] = false;
+                }
+            }
+        });
+        return result[0];
+    }
+    public boolean updateBookInStock(String id,int minus){
+        final boolean[] result = new boolean[1];
+        myRef = database.getReference("Books");
+        myRef.child(id).child("inStock").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int oldInStock = snapshot.getValue(Integer.class);
+                int newInStocck = oldInStock - minus;
+                myRef.child(id).child("inStock").setValue(newInStocck).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        result[0]=true;
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                result[0] = false;
+            }
+        });
+        return  result[0];
+    }
 }
+
+
