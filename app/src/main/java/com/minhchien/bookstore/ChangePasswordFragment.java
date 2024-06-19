efaaa908a4ae0fcd86110943497d53f60bf66682
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-06-19 17:57:14 +0700

    Sua giao dien va code chuc nang Doi Mat Khau

--- app/src/main/java/com/minhchien/bookstore/ChangePasswordFragment.java
+++ app/src/main/java/com/minhchien/bookstore/ChangePasswordFragment.java
@@ -7,58 +7,100 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ChangePasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.minhchien.bookstore.sharepreference.Constants;
+import com.minhchien.bookstore.sharepreference.PreferenceManager;
+
+
 public class ChangePasswordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    EditText txtMKrecent, txtNewMK, txtNewMKXacNhan;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button btnSave, btnBack;
 
-    public ChangePasswordFragment() {
-        // Required empty public constructor
-    }
+    private String phone, pass;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChangePasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChangePasswordFragment newInstance(String param1, String param2) {
-        ChangePasswordFragment fragment = new ChangePasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    DatabaseReference referencer;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public ChangePasswordFragment(){
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_change_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_change_password, container, false);
+        txtMKrecent = view.findViewById(R.id.changePass_pass);
+        txtNewMK = view.findViewById(R.id.changePass_newPass);
+        txtNewMKXacNhan = view.findViewById(R.id.changePass_newPassXacNhan);
+        btnSave = view.findViewById(R.id.changePass_btnSave);
+        btnBack = view.findViewById(R.id.changePass_btnBack);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updatePassword();
+            }
+        });
+        return view;
     }
+
+    private void updatePassword() {
+
+        // readData de doc mat khau hien tai tu User tu database
+        readData(new MyCallBack() {
+            @Override
+            public void onCallback(String value) {
+                PreferenceManager preferenceManager = new PreferenceManager(getContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+                phone = preferenceManager.getString(Constants.LOGIN_PHONE);
+                referencer = FirebaseDatabase.getInstance().getReference("Users");
+                pass = value;
+                // So sanh mat khau hien tai voi mat khau User nhap
+                if (pass.equals(txtMKrecent.getText().toString())) {
+                    String newPass = txtNewMK.getText().toString();
+                    String confirmNewPass = txtNewMKXacNhan.getText().toString(); // Lấy mật khẩu xác nhận
+
+                    if (newPass.length() >= 6 && newPass.equals(confirmNewPass)) { // Kiểm tra độ dài và sự khớp nhau
+                        referencer.child(phone).child("password").setValue(newPass);
+                        Toast.makeText(getActivity(), "Thay đổi mật khẩu thành công!", Toast.LENGTH_SHORT).show();
+                    } else if (newPass.length() < 6) {
+                        Toast.makeText(getActivity(), "Vui lòng nhập mật khẩu dài hơn 6 ký tự!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Mật khẩu mới và mật khẩu xác nhận không khớp!", Toast.LENGTH_SHORT).show(); // Thông báo lỗi
+                    }
+                }
+                else {
+                    Toast.makeText(getActivity(), "Thay đổi mật khẩu thất bại!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    // readData doc so dien thoai cua nguoi dung tu PreferenManager
+
+    public void readData(MyCallBack myCallback) {
+        PreferenceManager preferenceManager = new PreferenceManager(getContext(), Constants.LOGIN_KEY_PREFERENCE_NAME);
+        phone = preferenceManager.getString(Constants.LOGIN_PHONE);
+        referencer = FirebaseDatabase.getInstance().getReference("Users");
+        referencer.child(phone).child("password").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                myCallback.onCallback(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+    }
+
 }
