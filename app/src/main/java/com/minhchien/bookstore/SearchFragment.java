6f16fc8ba6e883d868fe0f014c6cb1c0f5624a72
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-07-01 14:07:43 +0700

    Hoan Thanh SearchFragment.java              14:07 1/7/2024

--- app/src/main/java/com/minhchien/bookstore/SearchFragment.java
+++ app/src/main/java/com/minhchien/bookstore/SearchFragment.java
@@ -2,63 +2,119 @@ package com.minhchien.bookstore;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.minhchien.bookstore.customadapter.CategoryAdapter;
+import com.minhchien.bookstore.model.Book;
+import com.minhchien.bookstore.model.Category;
+import com.minhchien.bookstore.ui.FilterCustomDialog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class SearchFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    List<Category> mListCategorys;
+    List<Book> listBooks;
+    CategoryAdapter categoryAdapter;
+    LinearLayout filter;
+    SearchView searchView;
+    RecyclerView mainRecyclerView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Books");
+        mListCategorys = new ArrayList<Category>();
+        listBooks = new ArrayList<>();
+        //data biding
+        searchView = rootView.findViewById(R.id.search);
+        mainRecyclerView = (RecyclerView) rootView.findViewById(R.id.main_recycleView);
+        filter = (LinearLayout) rootView.findViewById(R.id.filter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false);
+        mainRecyclerView.setLayoutManager(linearLayoutManager);
+        FragmentManager fragmentManager = getParentFragmentManager();
+        categoryAdapter = new CategoryAdapter(getContext(),fragmentManager,false);
+        mainRecyclerView.setAdapter(categoryAdapter);
+        handleFilter();
+        handleSearch();
+        return rootView;
+    }
+    private void queryDB(String search){
+        Query query = database.getReference("Books")
+                .orderByChild("title").startAt(search);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(listBooks != null){
+                    listBooks.clear();
+                }
+                if(mListCategorys != null){
+                    mListCategorys.clear();
+                }
+                if(snapshot.exists()){
+                    for (DataSnapshot data:snapshot.getChildren()){
+                        Book b = data.getValue(Book.class);
+                        if(b.getIsActive() == 1 && b.getInStockBook() > 0)
+                            listBooks.add(b);
+                    }
+                    mListCategorys.add(new Category(null,listBooks));
+                    categoryAdapter.setData(mListCategorys);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void handleFilter(){
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                FilterCustomDialog filterCustomDialog = FilterCustomDialog.newInstance();
+                filterCustomDialog.setCategoryAdapter(categoryAdapter);
+                filterCustomDialog.show(fragmentManager,"test dialog");
+            }
+        });
+    }
+    private void handleSearch(){
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                //queryDB(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                queryDB(s);
+                return false;
+            }
+        });
     }
 }
